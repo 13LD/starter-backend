d3241d524626fdcd170614d92f924b92f529cd80
13LD <tompla96@ukr.net>
2017-04-21 22:27:48 +0300

    last version, need to add keys for oauth on future

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules  :confirmable.
-  devise :database_authenticatable, :registerable,
+
+  devise  :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
           :omniauthable
+        
   include DeviseTokenAuth::Concerns::User
   has_one :role
   after_create :send_confirmation_email, if: -> { !Rails.env.test? && User.devise_modules.include?(:confirmable) }
