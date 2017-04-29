80c0e2292c604d61b7f013d3d5835e997301aeb7
13LD <tompla96@ukr.net>
2017-04-29 12:58:41 +0300

    oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, "824688744345552", "2d3b81dd6105c26a1bbf94677852561f"
+  provider :google_oauth2, "895506813976-urgi8tgmpddkf2h2kqirhknd8kuh54kr.apps.googleusercontent.com",   "viclVjieNIutMteARG7mmcy7"
 end
