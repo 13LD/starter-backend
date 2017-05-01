26bc0caa310abaa9e0f9b9a93bea616d59d7390c
13LD <tompla96@ukr.net>
2017-05-01 13:23:32 +0300

    fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, "824688744345552", "2d3b81dd6105c26a1bbf94677852561f", scope: 'email'
+  provider :facebook, "824688744345552", "2d3b81dd6105c26a1bbf94677852561f",info_fields: 'first_name,last_name,email',
+scope: 'public_profile,email',
+image_size: 'large',
+client_options: {
+site: 'https://graph.facebook.com/v2.6',
+authorize_url: 'https://www.facebook.com/v2.6/dialog/oauth'
+}
   provider :google_oauth2, "895506813976-urgi8tgmpddkf2h2kqirhknd8kuh54kr.apps.googleusercontent.com",   "viclVjieNIutMteARG7mmcy7"
 end
