4974563e05b7a4db08c6dac4d058c4275b942ca8
13LD <tompla96@ukr.net>
2017-05-01 11:43:38 +0300

    changes callback

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -2,14 +2,14 @@ Devise.setup do |config|
   # The e-mail address that mail will appear to be sent from
   # If absent, mail is sent from "please-change-me-at-config-initializers-devise@example.com"
   config.mailer_sender = "mailboxtrack@gmail.com"
-# Rails.application.config.to_prepare do              # to_prepare ensures that the monkey patching happens before the first request
-#   Devise::OmniauthCallbacksController.class_eval do # reopen the class
-#     def failure                                     # redefine the failure method
-#       set_flash_message! :alert, :failure, kind: OmniAuth::Utils.camelize(failed_strategy.name), reason: failure_message
-#       redirect_to after_omniauth_failure_path_for(resource_name)
-#     end
-#   end
-# end
+Rails.application.config.to_prepare do              # to_prepare ensures that the monkey patching happens before the first request
+  Devise::OmniauthCallbacksController.class_eval do # reopen the class
+    def failure                                     # redefine the failure method
+      set_flash_message! :alert, :failure, kind: OmniAuth::Utils.camelize(failed_strategy.name), reason: failure_message
+      redirect_to after_omniauth_failure_path_for(resource_name)
+    end
+  end
+end
   # If using rails-api, you may want to tell devise to not use ActionDispatch::Flash
   # middleware b/c rails-api does not include it.
   # See: http://stackoverflow.com/q/19600905/806956
