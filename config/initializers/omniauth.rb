d1bf059e14151df62436a913c6f114c506909ce7
13LD <tompla96@ukr.net>
2017-05-05 20:54:56 +0300

    check http status 405 facebook

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 OmniAuth.config.full_host = Rails.env.production? ? 'https://fathomless-bastion-13925.herokuapp.com/' : 'http://localhost:3000'
 
   provider :facebook, "824688744345552", "2d3b81dd6105c26a1bbf94677852561f",
-  callback_url: 'https://fathomless-bastion-13925.herokuapp.com/omniauth/facebook/callback'
+  callback_url: 'http://fathomless-bastion-13925.herokuapp.com/omniauth/facebook/callback'
 
   provider :google_oauth2, "895506813976-urgi8tgmpddkf2h2kqirhknd8kuh54kr.apps.googleusercontent.com",   "viclVjieNIutMteARG7mmcy7",
   {
