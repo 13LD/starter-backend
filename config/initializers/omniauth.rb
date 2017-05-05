ae24fd1fc5973e96b36ff10df634894e4c5e66eb
13LD <tompla96@ukr.net>
2017-05-05 19:18:23 +0300

    check http status 405 facebook

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,17 +2,10 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 OmniAuth.config.full_host = Rails.env.production? ? 'https://fathomless-bastion-13925.herokuapp.com/' : 'http://localhost:3000'
 
   provider :facebook, "824688744345552", "2d3b81dd6105c26a1bbf94677852561f",
-    scope: 'email',
-    secure_image_url: true,
-    auth_type: 'https',
-    info_fields: 'email,name',
-    client_options: {
-        site: "https://graph.facebook.com/v2.3",
-        authorize_url: "https://www.facebook.com/v2.3/dialog/oauth"
-    },
-    token_params: {
-        parse: :json
-    }
+  scope: 'public_profile, publish_actions, user_about_me',
+  info_fields: 'id, email, first_name, last_name, link, picture',
+  image_size: {:width => 165, :height => 165}
+  
   provider :google_oauth2, "895506813976-urgi8tgmpddkf2h2kqirhknd8kuh54kr.apps.googleusercontent.com",   "viclVjieNIutMteARG7mmcy7",
   {
 	prompt: 'select_account consent',
